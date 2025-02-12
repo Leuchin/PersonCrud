68c3ada44bca0b9407090db984ca82db662bbda3
Luiz Daniel <113204394+LuizDanielOliveira@users.noreply.github.com>
2025-02-12 18:12:34 -0300

    Update

--- Backend/obj/Debug/net9.0/PersonCRUD.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/PersonCRUD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbaa3e54ee554ee670299ef5b26a889b7758369c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6e0873bbb2f95ec9608d2964cf0305cd89dda9")]
 [assembly: System.Reflection.AssemblyProductAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PersonCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
